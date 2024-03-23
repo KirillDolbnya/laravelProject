ca313cc7d78f54b518668ae14b1b2ffa37c50341
dolbnakirill <kirilldolbnya6@gmail.com>
2024-03-23 22:06:52 +0300

    один ко многим, обновление таблицы

--- app/Console/Commands/DevCommand.php
+++ app/Console/Commands/DevCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Position;
 use App\Models\Profile;
 use App\Models\Worker;
 use Illuminate\Console\Command;
@@ -30,17 +31,31 @@ class DevCommand extends Command
 
 //        $this->prepareData();
 
-        $profile = Profile::find(1);
+
+
+
+//        $profile = Profile::find(1);
+//        dd($worker->profile->toArray());
 
         $worker = Worker::find(1);
+        $position = Position::find(2);
 
-        dd($worker->profile->toArray());
+
+        dd($position->workers->toArray());
+//        dd($worker->position->toArray());
 
         return 0;
     }
 
     private function prepareData()
     {
+        $position1 = Position::create([
+            'title' => 'Back-end'
+        ]);
+        $position2 = Position::create([
+            'title' => 'Designer'
+        ]);
+
         $workerData = [
             'name'=>'Kirill',
             'surname'=>'Dolbnya',
@@ -48,9 +63,65 @@ class DevCommand extends Command
             'age'=>20,
             'description'=>'im Kirill',
             'is_married'=>false,
+            'position_id'=>$position1->id,
+        ];
+
+        $workerData2 = [
+            'name'=>'Vlad',
+            'surname'=>'Romanenko',
+            'email' => 'Vlad@gmail.com',
+            'age'=>20,
+            'description'=>'im Vlad',
+            'is_married'=>false,
+            'position_id' => $position1->id,
+        ];
+
+        $workerData3 = [
+            'name'=>'Dima',
+            'surname'=>'Hilov',
+            'email' => 'Dima@gmail.com',
+            'age'=>19,
+            'description'=>'im Dima',
+            'is_married'=>true,
+            'position_id' => $position2->id,
+        ];
+
+        $workerData4 = [
+            'name'=>'Daniil',
+            'surname'=>'Smirnov',
+            'email' => 'Daniil@gmail.com',
+            'age'=>20,
+            'description'=>'im Daniil',
+            'is_married'=>false,
+            'position_id'=>$position2->id,
+        ];
+
+        $workerData5 = [
+            'name'=>'Nikita',
+            'surname'=>'Ivanov',
+            'email' => 'Nikita@gmail.com',
+            'age'=>20,
+            'description'=>'im Nikita',
+            'is_married'=>false,
+            'position_id' => $position1->id,
+        ];
+
+        $workerData6 = [
+            'name'=>'Nikita',
+            'surname'=>'Biev',
+            'email' => 'Biev@gmail.com',
+            'age'=>19,
+            'description'=>'im Nikita',
+            'is_married'=>true,
+            'position_id' => $position2->id,
         ];
 
         $worker = Worker::create($workerData);
+        $worker2 = Worker::create($workerData2);
+        $worker3 = Worker::create($workerData3);
+        $worker4 = Worker::create($workerData4);
+        $worker5 = Worker::create($workerData5);
+        $worker6 = Worker::create($workerData6);
 
         $profileData = [
             'worker_id'=>$worker->id,
@@ -60,8 +131,51 @@ class DevCommand extends Command
             'finished_study_at'=>'2020-06-01',
         ];
 
-        $profile = Profile::create($profileData);
+        $profileData2 = [
+            'worker_id'=>$worker2->id,
+            'city'=>'Stavropol',
+            'skill'=>'Coder',
+            'experience'=>1,
+            'finished_study_at'=>'2021-06-01',
+        ];
 
-        dd($profile->id);
+        $profileData3 = [
+            'worker_id'=>$worker3->id,
+            'city'=>'Stavropol',
+            'skill'=>'Coder',
+            'experience'=>1,
+            'finished_study_at'=>'2022-06-01',
+        ];
+
+        $profileData4 = [
+            'worker_id'=>$worker4->id,
+            'city'=>'Oslo',
+            'skill'=>'Coder',
+            'experience'=>5,
+            'finished_study_at'=>'2020-06-01',
+        ];
+
+        $profileData5 = [
+            'worker_id'=>$worker5->id,
+            'city'=>'New-York',
+            'skill'=>'Designer',
+            'experience'=>1,
+            'finished_study_at'=>'2021-06-01',
+        ];
+
+        $profileData6 = [
+            'worker_id'=>$worker6->id,
+            'city'=>'London',
+            'skill'=>'Manager',
+            'experience'=>1,
+            'finished_study_at'=>'2022-06-01',
+        ];
+
+        Profile::create($profileData);
+        Profile::create($profileData2);
+        Profile::create($profileData3);
+        Profile::create($profileData4);
+        Profile::create($profileData5);
+        Profile::create($profileData6);
     }
 }
---
+++ app/Models/Position.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Position extends Model
+{
+    use HasFactory;
+
+    protected $guarded = false;
+
+    public function workers()
+    {
+        return $this->hasMany(Worker::class,'position_id','id');
+    }
+}
