9990873adc2db0db2137e7e121ab2c8b5205964f
dolbnakirill <kirilldolbnya6@gmail.com>
2024-04-18 02:27:49 +0300

    route resource

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/workers',[\App\Http\Controllers\WorkerController::class,'index'])->name('worker.index');
-Route::get('/workers/create',[\App\Http\Controllers\WorkerController::class,'create'])->name('worker.create');
-Route::post('/workers',[\App\Http\Controllers\WorkerController::class,'store'])->name('worker.store');
-Route::get('/workers/{worker}/edit',[\App\Http\Controllers\WorkerController::class,'edit'])->name('worker.edit');
-Route::patch('/workers/{worker}',[\App\Http\Controllers\WorkerController::class,'update'])->name('worker.update');
-Route::delete('/workers/{worker}',[\App\Http\Controllers\WorkerController::class,'delete'])->name('worker.delete');
-Route::get('/workers/{worker}',[\App\Http\Controllers\WorkerController::class,'show'])->name('worker.show');
+Route::resource('workers',\App\Http\Controllers\WorkerController::class);
+
+//Route::get('/workers',[\App\Http\Controllers\WorkerController::class,'index'])->name('worker.index');
+//Route::get('/workers/create',[\App\Http\Controllers\WorkerController::class,'create'])->name('worker.create');
+//Route::post('/workers',[\App\Http\Controllers\WorkerController::class,'store'])->name('worker.store');
+//Route::get('/workers/{worker}/edit',[\App\Http\Controllers\WorkerController::class,'edit'])->name('worker.edit');
+//Route::patch('/workers/{worker}',[\App\Http\Controllers\WorkerController::class,'update'])->name('worker.update');
+//Route::delete('/workers/{worker}',[\App\Http\Controllers\WorkerController::class,'delete'])->name('worker.delete');
+//Route::get('/workers/{worker}',[\App\Http\Controllers\WorkerController::class,'show'])->name('worker.show');
+//Route::get('/edit',[ \App\Filament\Resources\UserResource\Pages\Settings::class,'getHeaderActions'])->name('posts.edit');
